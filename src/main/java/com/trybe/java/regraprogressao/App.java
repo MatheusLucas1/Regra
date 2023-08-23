6b311b0d490f5db7b44766a33201917e672de20b
Matheus <Matheus.lucas1105@gmail.com>
2023-08-23 08:55:49 -0300

    Cadastra atividades com sucesso

--- src/main/java/com/trybe/java/regraprogressao/App.java
+++ src/main/java/com/trybe/java/regraprogressao/App.java
@@ -1,5 +1,7 @@
 package com.trybe.java.regraprogressao;
 
+import java.util.Scanner;
+
 /**
  * App.
  */
@@ -9,6 +11,16 @@ public class App {
    * Metodo main.
    */
   public static void main(String[] args) {
-
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("Digite a quantidade de atividades para cadastrar:");
+    String quantidadeString = scanner.next();
+    int quantidadeNum = Integer.parseInt(quantidadeString);
+    for (int i = 1; i <= quantidadeNum; i++) {
+      System.out.println("Digite o nome da atividade " + i + ":");
+      String nomeAtividade = scanner.next();
+      System.out.println("Digite o peso da atividade " + i + ":");
+      String pesoAtividade = scanner.next();
+    }
   }
+
 }
